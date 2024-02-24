31ed6e1e3e5d0e6591aff9c01ae7b8a3029ce39e
Garrrruii <hishally012@gmail.com>
2024-02-24 21:13:56 +0900

    feat: Make items function and page

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package hello.itemservice.web.basic;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+	private final ItemRepository itemRepository;
+
+	@GetMapping
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+
+		return "basic/items";
+	}
+
+	@PostConstruct
+	public void init() {
+		itemRepository.save(Item.builder().name("itemA").price(10000).quantity(10).build());
+		itemRepository.save(Item.builder().name("itemB").price(20000).quantity(20).build());
+	}
+
+}
