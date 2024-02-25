4c8a7b1d5a3065658c9c160ce49c9ffd2dce9568
Garrrruii <hishally012@gmail.com>
2024-02-25 19:11:18 +0900

    feat: Make validation checking

--- src/main/java/hello/itemservice/web/validation/ValidationItemController.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemController.java
@@ -1,9 +1,12 @@
 package hello.itemservice.web.validation;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,7 +48,14 @@ public class ValidationItemController {
 	}
 
 	@PostMapping("/add")
-	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+		Map<String, String> errors = getErrors(item);
+		if (!errors.isEmpty()) {
+			log.info("{}", errors);
+			model.addAttribute("errors", errors);
+			return "validation/addForm";
+		}
+
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
 		redirectAttributes.addAttribute("status", true);
@@ -65,5 +75,29 @@ public class ValidationItemController {
 		return "redirect:/validation/items/{itemId}";
 	}
 
+	private static Map<String, String> getErrors(Item item) {
+		Map<String, String> errors = new HashMap<>();
+
+		// 필드 검증
+		if (!StringUtils.hasText(item.getName())) {
+			errors.put("name", "상품명은 필수입니다.");
+		}
+		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+			errors.put("price", "가격은 1,000~1,000,000까지 허용합니다.");
+		}
+		if (item.getQuantity() == null || item.getQuantity() < 0 || item.getQuantity() > 9999) {
+			errors.put("quantity", "수량은 1~9,999까지 허용합니다.");
+		}
+
+		// 복합 룰 검증
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			long value = (long)item.getPrice() * item.getQuantity();
+			if (value < 10000L) {
+				errors.put("global", "가격*수량은 10,000 이상이어야 합니다. 현재값 = " + value);
+			}
+		}
+
+		return errors;
+	}
 }
 
