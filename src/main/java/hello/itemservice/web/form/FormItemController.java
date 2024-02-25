5a72a0b5a0b16a6f06680b0991d7a20a7ab0f1c5
Garrrruii <hishally012@gmail.com>
2024-02-25 16:44:51 +0900

    feat: Add 배송방식 (select)

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -42,6 +44,15 @@ public class FormItemController {
 		return ItemType.values();
 	}
 
+	@ModelAttribute("deliveryCodes")
+	public List<DeliveryCode> deliveryCodes() {
+		List<DeliveryCode> deliveryCodes = new ArrayList<>();
+		deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+		deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+		deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+		return deliveryCodes;
+	}
+
 	@GetMapping
 	public String items(Model model) {
 		List<Item> items = itemRepository.findAll();
@@ -64,7 +75,7 @@ public class FormItemController {
 
 	@PostMapping("/add")
 	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-		log.info("{}", item.getItemType());
+		log.info("{}", item.getDeliveryCode());
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
 		redirectAttributes.addAttribute("status", true);
