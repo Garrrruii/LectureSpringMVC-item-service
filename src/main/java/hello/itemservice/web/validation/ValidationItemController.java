1725404e6fe365f072853e49418bf02cbdd55aa1
Garrrruii <hishally012@gmail.com>
2024-02-25 18:54:56 +0900

    feat: Init validation project

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemController.java
@@ -0,0 +1,69 @@
+package hello.itemservice.web.validation;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation/items") // v1
+@RequiredArgsConstructor
+public class ValidationItemController {
+
+	private final ItemRepository itemRepository;
+
+	@GetMapping
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+		return "validation/items";
+	}
+
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/item";
+	}
+
+	@GetMapping("/add")
+	public String addForm(Model model) {
+		model.addAttribute("item", Item.builder().build());
+		return "validation/addForm";
+	}
+
+	@PostMapping("/add")
+	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/items/{itemId}";
+	}
+
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/editForm";
+	}
+
+	@PostMapping("/{itemId}/edit")
+	public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+		itemRepository.update(itemId, item);
+		return "redirect:/validation/items/{itemId}";
+	}
+
+}
+
