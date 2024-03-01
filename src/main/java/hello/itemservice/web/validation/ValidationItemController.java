a3bfe003d26b380f0743fc910dc257ccc881a7d6
Garrrruii <hishally012@gmail.com>
2024-03-01 15:34:58 +0900

    feat: Use error code level and rejectValue

--- src/main/java/hello/itemservice/web/validation/ValidationItemController.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemController.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,6 +50,9 @@ public class ValidationItemController {
 	public String addItem(@ModelAttribute Item item, BindingResult bindingResult,
 		RedirectAttributes redirectAttributes) {
 		// item과 bindingResult의 순서가 중요 (item에 바인딩 실패 시 bindingResult에 FieldError를 담음)
+		// bindingResult는 본인이 검증할 대상이 무엇인지 알고 있음
+		log.info("objectName={} target={}", bindingResult.getObjectName(), bindingResult.getTarget());
+
 		validateItem(item, bindingResult);
 		if (bindingResult.hasErrors()) {
 			log.info("{}", bindingResult);
@@ -80,27 +81,23 @@ public class ValidationItemController {
 	private static void validateItem(Item item, BindingResult bindingResult) {
 		// 필드 검증
 		if (!StringUtils.hasText(item.getName())) {
-			bindingResult.addError(new FieldError(
-				"item", "name", item.getName(), false,
-				new String[] {"required.item.itemName"}, null, null));
+			bindingResult.rejectValue("name", "required");
 		}
+		// ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "name", "required");
+		// 위 if문이 이 코드와 동일 (단순 기능만 있음)
+
 		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-			bindingResult.addError(new FieldError(
-				"item", "price", item.getPrice(), false,
-				new String[] {"range.item.price"}, new Object[] {1000, 1000000}, null));
+			bindingResult.rejectValue("price", "range", new Object[] {1000, 1000000}, null);
 		}
 		if (item.getQuantity() == null || item.getQuantity() < 1 || item.getQuantity() > 9999) {
-			bindingResult.addError(new FieldError(
-				"item", "quantity", item.getQuantity(), false,
-				new String[] {"max.item.quantity"}, new Object[] {1, 9999}, null));
+			bindingResult.rejectValue("quantity", "range", new Object[] {1, 9999}, null);
 		}
 
 		// 복합 룰 검증
 		if (item.getPrice() != null && item.getQuantity() != null) {
 			long value = (long)item.getPrice() * item.getQuantity();
 			if (value < 10000L) {
-				bindingResult.addError(new ObjectError(
-					"item", new String[] {"totalPriceMin"}, new Object[] {10000, value}, null));
+				bindingResult.reject("totalPriceMin", new Object[] {10000, value}, null);
 			}
 		}
 	}
